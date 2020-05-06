237e4c22bd55eb85769c1f4f0614c11e15b6f805
Gael Huot <gael.huot@livdeo.fr>
2020-05-06 19:15:59 +0200

    Initial commit

---
+++ src/midi-footswitch-controller.cpp
@@ -0,0 +1,12 @@
+﻿// midi-footswitch-controller.cpp : définit le point d'entrée de l'application.
+//
+
+#include "midi-footswitch-controller.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
