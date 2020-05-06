43a423a9dc7bf73787c2b489b8c7ad63c1414314
Gael Huot <gael.huot@livdeo.fr>
2020-05-06 22:56:28 +0200

    Code comments

--- src/midi-footswitch-controller.cpp
+++ src/midi-footswitch-controller.cpp
@@ -12,60 +12,80 @@
 
 using namespace std;
 
-int** data;
-
 int main()
 {
 	cout << "Bytes sizes : " << _BSIZE << endl;
+
+    // Just to start working, we just put databytes into a simple file
 	read_exampledt("C:\\Users\\ghuot\\arduino-midi\\midi-footswitch-controller\\src\\example.dt");
 	return 0;
 }
 
 void read_exampledt(string filename) {
     string bytes;
-
     ifstream in;
+
     in.open(filename);
 
     if (in.good()) {
+        // First we read the file
         getline(in, bytes);
 
+        // We count the number of banks in order to allow memory
         int bankCount = bytes.length() / (SWITCH_COUNT * _BSIZE);
 
-        int** data = new int* [bankCount];
 
+        // Allocating memory
+        int** data = new int* [bankCount];
         for (int i = 0; i < bankCount; i++) {
             data[i] = new int[SWITCH_COUNT];
         }
 
-        int* currentByte;
-        int currentByteCursor = 0;
+        // Usefull variables
+        int* currentByte; // The current "command" readed
+        int currentByteCursor = 0; // The current command readed cursot
 
-        int currentBankCursor = 0;
-        int currentSwitchCursor = 0;
+        int currentBankCursor = 0; // Current bank cursor
+        int currentSwitchCursor = 0; // Current switch (in the bank) cursor
+
+        // Let's start parsing
         for (int i = 0; i < bytes.length() - _BSIZE; i++) {
+
+            // Reset the currentByte if the cursor is at zero
             if (currentByteCursor == 0) {
                 currentByte = new int[_BSIZE];
             }
 
+            // Parsing the char from bytes into an int.
+            // The char is gonna be "5", but we want the real "5" number, and not the ascii code of "5"
             currentByte[currentByteCursor] = (int)bytes[i] - '0';
+            
+            // Incrementing the cursor
             currentByteCursor++;
 
+            // Current byte is full, adding it to data
             if (currentByteCursor >= _BSIZE) {
+
+                // Here, we calculate the current command int.
+                // We got something like {1,2,7}. We just have to do (1 * 10^2) + (2 * 10^1) + (7 * 10^0)
                 int byteForSwitch = 0;
                 for (int j = 0; j < _BSIZE; j++) {
                     byteForSwitch += pow(10, _BSIZE - j - 1) * currentByte[j];
                 }
 
-                cout << currentBankCursor << " - " << currentSwitchCursor << " - " << byteForSwitch << endl;
+                // Associate the command to the switch in current bank
                 data[currentBankCursor][currentSwitchCursor] = byteForSwitch;
 
+                // Going to next switch
                 currentSwitchCursor++;
+                
+                // Reached switchs limit, going to next bank and reseting switch
                 if (currentSwitchCursor >= SWITCH_COUNT) {
                     currentBankCursor++;
                     currentSwitchCursor = 0;
                 }
 
+                // Reseting the currentByteCursor
                 currentByteCursor = 0;
             }
         }
@@ -77,5 +97,4 @@ void read_exampledt(string filename) {
         cout << "error reading" << endl;
 
     in.close();
-
 }
